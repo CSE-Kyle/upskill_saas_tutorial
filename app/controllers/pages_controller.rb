f7e35c03f9b3d6cb002a7ea9b55405c9444d14b2
Kyle Deguzman <kyledeguzman147@yahoo.com>
2021-06-15 02:25:00 +0000

    Added about view, route, controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
     def home # function name must match the file name on "pages" directory
     end
     
-    def about 
-        
+    def about # linked to about route in routes.rb (get 'about', to: 'pages#about')
     end
+    
 end 
