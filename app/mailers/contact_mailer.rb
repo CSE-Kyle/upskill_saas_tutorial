74138b225ee99ef780146723269bb48edf1a1b45
Kyle Deguzman <kyledeguzman147@yahoo.com>
2021-07-05 01:10:57 +0000

    modified file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base 
-  default to: 'kyledeguzman86@gmail.com'
+  default to: 'kyledeguzman147@gmail.com'
   
   def contact_email(name, email, body) 
     @name = name 
